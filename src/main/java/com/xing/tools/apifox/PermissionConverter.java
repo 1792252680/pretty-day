3f7a4f9bc28c605f075534a7b1436d188e66bccd
xing <1792252680@qq.com>
2022-05-27 10:25:22 +0800

    update

--- src/main/java/com/xing/tools/apifox/PermissionConverter.java
+++ src/main/java/com/xing/tools/apifox/PermissionConverter.java
@@ -2,39 +2,69 @@ package com.xing.tools.apifox;
 
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONObject;
+import com.xing.mysql.PermissionConn;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * 根据ApiFox导出文档生成权限表
+ */
 public class PermissionConverter {
-    // id, method, name, parentId, permExpression, url
-    static String insertApiSqlTemplate = "INSERT INTO permission " +
-            "(id, createBy, status, updateBy, method, name, permExpression, sort, type, url)" +
-            " VALUES " +
-            "({}, 'system', 1, 'system', {}, {}, {}, 0, 'API', {});";
-    static List<String> existList = ListUtil.of("file:upload:post", "image:apply:delete", "image:apply:feature:delete", "image:apply:feature:put", "image:apply:get", "image:apply:post", "image:apply:put", "image:apply:ship:delete", "image:apply:ship:detail:get", "image:apply:ship:get", "image:apply:ship:post", "image:apply:ship:put", "image:dynamic:snap:batch:delete", "image:dynamic:snap:batch:put", "image:dynamic:snap:delete", "image:dynamic:snap:detail:get", "image:dynamic:snap:get", "image:dynamic:snap:put", "image:ship:rcg:import:post", "image:ship:rcg:post", "image:ship:search:post", "image:standard:batch:delete", "image:standard:delete", "image:standard:detail:get", "image:standard:get", "image:standard:post", "image:standard:put", "ship:control:get", "ship:control:manual:delete", "ship:control:manual:detail:get", "ship:control:manual:get", "ship:control:manual:post", "ship:control:manual:put", "ship:warn:delete", "ship:warn:export:get", "ship:warn:get", "snap:address:delete", "snap:address:get", "snap:address:post", "snap:address:put", "user:delete", "user:get", "user:logout:post", "user:org:delete", "user:org:get", "user:org:post", "user:org:put", "user:permission:get", "user:post", "user:profile:get", "user:profile:post", "user:put");
+    /**
+     * 根据项目动态设置数据库名称
+     */
+    static List<String> existPermExpressionList =
+            PermissionConn.getPermExpressionList("bianjian_api");
+
+    // 插入会插入permission. id, method, name, parentId, permExpression, url
+    static String insertApiSqlTemplate =
+            "INSERT INTO permission " +
+                    "(id, createBy, status, updateBy, method, name, permExpression, sort, type, url)" +
+                    " VALUES " +
+                    "({}, 'system', 1, 'system', {}, {}, {}, 0, 'API', {});";
+    // 更新只更新 permission.name(名字)
+    static String updateApiSqlTemplate =
+            "UPDATE permission SET name={} WHERE permExpression = {};";
+
+
+    static Boolean isInsert = false;
 
     public static void main(String[] args) throws IOException {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        FileSystemView view = FileSystemView.getFileSystemView();
+        File file = view.getHomeDirectory();
 
-        Files.list(Paths.get("src/main/java/com/xing/tools/apifox"))
-                .filter(path -> path.toString().endsWith(".json"))
+        Files.list(Paths.get(file.getAbsolutePath()))
+                .filter(path -> path.toString().endsWith(".apifox.json"))
                 .forEach(filePath -> {
                     try {
+                        // ApiFox导出"忽略权限标签"后的json文件
                         ApiFoxApi apiFoxApi = JSONObject.parseObject(Files.readAllLines(filePath).get(0), ApiFoxApi.class);
                         ApiCollectionItem apiCollectionItem = apiFoxApi.getApiCollection().get(0);
                         List<ItemsItem> items = apiCollectionItem.getItems();
                         iteratorPrint(items);
-
+                        if (isInsert) {
+                            System.out.println(
+                                    "\n" +
+                                            "insert into role_permission(permissionId, roleId)\n" +
+                                            "select permissionId, 1\n" +
+                                            "from role_permission\n" +
+                                            "where permissionId not in ( select id from permission );"
+                            );
+                        }
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 });
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
     }
 
     private static String concatLeftAndRight(String column) {
@@ -50,7 +80,13 @@ public class PermissionConverter {
                 String permExpression = api.getPath()
                         .substring(1)
                         .replaceAll("/", ":") + ":" + api.getMethod();
-                if (existList.contains(permExpression)) continue;
+                if (existPermExpressionList.contains(permExpression)) {
+                    String updateSql = StrUtil.format(updateApiSqlTemplate,
+                            concatLeftAndRight(item.getName()),
+                            concatLeftAndRight(permExpression));
+                    System.out.println(updateSql);
+                    continue;
+                }
                 String insertSql = StrUtil.format(insertApiSqlTemplate,
                         api.getId(),
                         concatLeftAndRight(api.getMethod().toUpperCase(Locale.ROOT)),
@@ -60,6 +96,7 @@ public class PermissionConverter {
                 );
 
                 System.out.println(insertSql);
+                isInsert = true;
             } else {
                 iteratorPrint(item.getItems());
             }
